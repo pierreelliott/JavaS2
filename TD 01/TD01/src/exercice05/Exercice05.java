4243c158e7c3d7db3403f1cfb9b6c214ec8fc359
unknown <p1402690@IUTDOUAB2PC133.univ-lyon1.fr>
2016-02-03 15:42:47 +0100

    Début TD01 Exo5

---
+++ TD 01/TD01/src/exercice05/Exercice05.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercice05;
+
+/**
+ *
+ * @author p1402690
+ */
+public class Exercice05 {
+    
+    
+            public static void main(String[]args)
+            {
+                
+                java.util.Scanner entree =new java.util.Scanner(System.in);
+                System.out.println("Saissisez un montant en euro entier");
+                int montant = entree.nextInt();
+                
+                System.out.println("Vous avez saisi : "+montant);
+            }
+    
+}
